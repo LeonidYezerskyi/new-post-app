ab9d7616f288d015cf15a9bf5f2bb989da0651be
LeonidYezerskyi <111503058+LeonidYezerskyi@users.noreply.github.com>
2023-08-12 12:12:38 +0300

    ADDED: routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
+import { Suspense, lazy } from 'react';
+import { NavLink, Route, Routes } from 'react-router-dom';
 
-import './App.css'
+import css from './App.module.css'
+
+const LazyHomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const LazyOfficesPage = lazy(() => import("./pages/OfficesPage/OfficesPage"));
+const LazyNotFoundPage = lazy(() => import("./pages/NotFoundPage/NotFoundPage"));
 
 function App() {
 
   return (
-    <>
-      <div>
-        App
-      </div>
+    <div>
+      <nav className={css.header}>
+        <NavLink to='/' >
+          Home
+        </NavLink>
+        <NavLink to='/offices' >
+          List of offices
+        </NavLink>
+      </nav>
 
-    </>
+      <>
+        <Suspense>
+          <Routes >
+            <Route path="/" element={<LazyHomePage />} />
+            <Route path="/offices/*" element={<LazyOfficesPage />} />
+            <Route path="/*" element={<LazyNotFoundPage />} />
+          </Routes>
+        </Suspense>
+      </>
+    </div>
   )
 }
 
---
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -0,0 +1,9 @@
+import css from './NotFoundPage.module.css';
+
+const NotFoundPage = () => {
+    return (
+        <div className={css.notification}>Not Found Page</div>
+    )
+}
+
+export default NotFoundPage;
